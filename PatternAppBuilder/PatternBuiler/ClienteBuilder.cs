e2159e8bef7a2ed5011038848bf4a5e6bc027afe
Adeildo José de Oliveira <adeildo.joliveira@hotmail.com>
2018-07-17 23:50:51 -0300

    Subindo respositório

---
+++ PatternAppBuilder/PatternBuiler/ClienteBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PatternAppBuilder.PatternBuiler
+{
+    public class ClienteBuilder
+    {
+        private string _nome;
+        private string _sobreNome;
+        private string _email;
+        private string _telefone;
+        private string _endereco;
+        private int _numero;
+        private string _complemento;
+    }
+}
